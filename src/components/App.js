a1477182b05cf1c753431439c5c95a3b054f6b60
benjaminpettus <benjaminpettus@gmail.com>
2019-02-05 15:12:54 -0800

    FeaturedMix and Header components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import FeaturedMix from './FeaturedMix';
+import Header from './Header';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Marmalade.fm</h1>
+      <div>
+        <div>
+          { /*  FeaturedMix*/ }
+          <FeaturedMix />
+          <div>
+            {/* Header */}
+            <Header />
+            {/* Routed Page */}
+          </div>
+        </div>
+        {/* Audio Player */}
+        <iframe 
+          width="100%" 
+          height="60" 
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FPharcydeTV%2Fsplendidradio-w-octavio-camacho-and-mixmaster-wolf%2F" 
+          frameBorder="0" >
+        </iframe>
       </div>
     );
   }
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+const FeaturedMix = props => (
+    <div>
+        <h1>Featured Mix</h1>
+        <h1>Mint Condition w/ Hotthobo - 27th November 2017</h1>
+        {/* play button */}
+    </div>
+)
+
+
+
+export default FeaturedMix
